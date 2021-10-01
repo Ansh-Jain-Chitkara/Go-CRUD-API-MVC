abcfd1c03fd6ec881706185ac5020c99eb8eacc6
Ansh Jain <anshjain.cse18@chitkarauniversity.edu.in>
2021-10-01 16:36:51 +0530

    Update router.go

--- api/router/router.go
+++ api/router/router.go
@@ -9,11 +9,14 @@ import (
 func Router() *mux.Router {
 	router := mux.NewRouter()
 
+	router.HandleFunc("/login", controllers.Login).Methods("POST")
 	router.HandleFunc("/employee", controllers.GetEmployees).Methods("GET")
 	router.HandleFunc("/employee/{id}", controllers.GetEmployee).Methods("GET")
 	router.HandleFunc("/employee", controllers.CreateEmployee).Methods("POST")
 	router.HandleFunc("/employee/{id}", controllers.UpdateEmployee).Methods("PUT")
 	router.HandleFunc("/employee/{id}", controllers.DeleteEmployee).Methods("DELETE")
+	router.HandleFunc("/refresh", controllers.Refresh).Methods("POST")
+
 	return router
 
 }
