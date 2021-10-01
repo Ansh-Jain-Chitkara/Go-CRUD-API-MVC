fd161c415d298744fa17a4a9aceb5649e6f6a0f7
Ansh Jain <anshjain.cse18@chitkarauniversity.edu.in>
2021-10-01 16:37:34 +0530

    Update main.go

--- api/main.go
+++ api/main.go
@@ -10,16 +10,26 @@ import (
 func main() {
 
 	r := router.Router()
-	r.Use(CommonMiddleware)
+	r.Use(setHeaders)
 	fmt.Println("Starting server at port 8080")
-	//	start the server
+	//	Start the server
 	log.Fatal(http.ListenAndServe(":8080", r))
-
 }
-func CommonMiddleware(next http.Handler) http.Handler {
+func setHeaders(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Add("Content-Type", "application/json")
-		next.ServeHTTP(w, r)
+		//anyone can make a CORS request
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, DELETE, PUT")
+		//Since I was building a REST API that returned JSON, I set the content type to JSON here.
+		w.Header().Set("Content-Type", "application/json")
+		//Allow requests to have the following headers
+		w.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type, Authorization, cache-control")
+
+		if r.Method == "OPTIONS" {
+			return
+		}
+
+		h.ServeHTTP(w, r)
 	})
 }
 
